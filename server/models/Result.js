5ea1dda2a00683415896d14c6d0466fb282db04d
Nikhil Tripathy <nikhiltripathy30403@gmail.com.com>
2025-05-18 23:39:50 +0530

    moved package.json to src

--- server/models/Result.js
+++ server/models/Result.js
@@ -1,14 +1,4 @@
-// import mongoose from 'mongoose';
 
-// const resultSchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-//   accuracy: Number,
-//   wpm: Number,
-//   createdAt: { type: Date, default: Date.now }
-// });
-
-// const Result = mongoose.model('Result', resultSchema);
-// export default Result;
 
 
 // models/Result.js
--- server/models/Word.js
+++ server/models/Word.js
@@ -1,32 +1,4 @@
-// // import mongoose from 'mongoose';
 
-// // const wordSchema = new mongoose.Schema({
-// //   text: { type: String, required: true }
-// // });
-
-// // const Word = mongoose.model('Word', wordSchema);
-// // export default Word;
-
-
-// import mongoose from 'mongoose';
-
-// const wordSchema = new mongoose.Schema({
-//   text: {
-//     type: String,
-//     required: true,
-//     trim: true
-//   },
-//   type: {
-//     type: String,
-//     required: true,
-//     enum: ['word', 'punctuation', 'number', 'quote'],
-//     default: 'word'
-//   }
-// }, { timestamps: true });
-
-// const Word = mongoose.model('Word', wordSchema);
-
-// export default Word;
 
 
 
