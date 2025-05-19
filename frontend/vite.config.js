90495e5d601e2e7975ecfa0953702ffd6ea83095
Nikhil Tripathy <nikhiltripathy30403@gmail.com.com>
2025-05-19 15:31:15 +0530

    uodated package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': 'http://localhost:5000', // change if backend uses different port
+//       exclude: ['mock-aws-s3', 'aws-sdk', 'nock', '@mapbox/node-pre-gyp']
+//     }
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // The proxy configuration will only be used during development
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // change if backend uses different port
+      '/api': 'http://localhost:5000',
       exclude: ['mock-aws-s3', 'aws-sdk', 'nock', '@mapbox/node-pre-gyp']
     }
   }
